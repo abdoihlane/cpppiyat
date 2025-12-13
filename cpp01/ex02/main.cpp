3960826ff63a9fd96ae90ea33d4f93cb11af5668
abdoihlane <habibiihlanea1@@gmail.com>
2025-12-13 20:58:26 +0100

    habibi has finaly pushed

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -4,14 +4,15 @@
 int main()
 {
     std::string  ss = "HI THI IS BRAIN";
-    std::string *stringPTR = &ss;
-    std::string &stringREF = ss;
+    std::string *pointer = &ss;
+    std::string &REFERENCE = ss;
 
-    std::cout << &ss << std::endl;
-    std::cout << stringPTR << std::endl;
-    std::cout << &stringREF << std::endl;
-
-    std::cout << ss << std::endl;
-    std::cout << *stringPTR << std::endl;
-    std::cout << stringREF << std::endl;
+    std::cout << "The memory address of the string variable: " << &ss << std::endl;
+    std::cout << "The memory address held by stringPTR:      " << pointer << std::endl;
+    std::cout << "The memory address held by stringREF:      " << &REFERENCE << std::endl;
+    std::cout << "------------------------------------------------------" << std::endl;
+    std::cout << "The value of the string variable  : " << ss << std::endl;
+    std::cout << "The value pointed to by stringPTR : " << *pointer << std::endl;
+    std::cout << "The value pointed to by stringREF : " << REFERENCE << std::endl;
 }
+
